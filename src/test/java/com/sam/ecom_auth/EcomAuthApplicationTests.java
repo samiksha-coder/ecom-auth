78762221fc6312fe4c40a54782aba9b53ac9ad4a
Samiksha Goyal <samiksha.g17@gmail.com>
2024-10-12 18:15:27 +0530

    Init

---
+++ src/test/java/com/sam/ecom_auth/EcomAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.ecom_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
